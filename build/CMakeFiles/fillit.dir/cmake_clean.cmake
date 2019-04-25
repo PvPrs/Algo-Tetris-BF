411c9a551cf3d65cd08c5d2433d99dd606a7e501
= <dave_bochove@hotmail.com>
2019-04-25 17:27:44 +0200

    repackaging

---
+++ build/CMakeFiles/fillit.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fillit.dir/src/main.c.o"
+  "CMakeFiles/fillit.dir/src/sort_list.c.o"
+  "CMakeFiles/fillit.dir/src/reader.c.o"
+  "CMakeFiles/fillit.dir/src/validator.c.o"
+  "CMakeFiles/fillit.dir/src/solver.c.o"
+  "fillit.pdb"
+  "fillit"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/fillit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
